8694eccf28334153d0c1fe963122b1f54258bbe7
puh <puhovik@protonmail.com>
2022-09-10 20:11:24 +0300

    fix fmt

--- src/response.rs
+++ src/response.rs
@@ -4,21 +4,20 @@ pub struct Interface {
     pub address: IpAddr,
     pub private_key: String,
     pub listen_port: u16,
-    pub dns: IpAddr
+    pub dns: IpAddr,
 }
 
 pub struct Peer {
     pub public_key: String,
     pub endpoint: IpAddr,
-    pub allowed_ips: String
+    pub allowed_ips: String,
 }
-
 
 pub struct FullPeerConfig {
     pub interface: Interface,
-    pub peer: Peer
+    pub peer: Peer,
 }
 
 pub struct NewClientResponse {
-    pub config: String
+    pub config: String,
 }
